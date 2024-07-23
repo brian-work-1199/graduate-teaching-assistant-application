f04a67108d8566dab9ec4e88fc9793945e4712e0
Brian <thinhle1199@gmail.com>
2024-07-23 09:06:59 -0500

    [Front-end] remove redundant

--- back-end/src/routes/productRoutes.ts
+++ back-end/src/routes/productRoutes.ts
@@ -16,7 +16,7 @@ router.get('/products', async (req, res) => {
 router.get('/products/supplier/:id', async (req, res) => {
   const supplierId = parseInt(req.params.id);
   try {
-    const [rows] = await db.query('SELECT * FROM products WHERE supplierId = ?', [supplierId]);
+    const [rows] = await db.query('SELECT p.productId, p.name, p.unitPrice, p.quantityInStock, p.dateSupplied, s.name, s.contactPhone FROM products p JOIN suppliers s ON p.supplierId = s.supplierId WHERE s.supplierId = ?', [supplierId]);
     res.json(rows);
   } catch (err) {
     console.error(err);
